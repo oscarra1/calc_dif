2e3735233e61c841790a463238cc43e8ad50854f
Oscar Rendón Aldaraca <o_rendon@yahoo.com>
2017-03-24 07:46:01 -0600

    En la rama master  Cambios para hacer commit: 	modificado:    Ejercicios24-mar-17.pdf 	modificado:    Ejercicios24-mar-17.tex 	modificado:    ejercicios.pl
        termina 10 ejercicios

--- ejercicios.pl
+++ ejercicios.pl
@@ -83,6 +83,11 @@ if($exp1==2){
 else{
  $exp2="^".($exp1-1);
  $exp1="^".$exp1}
+
+ $exp3=int(rand(4)+2);
+ if($exp3==2){
+  $exp3=""}
+
 $sig1=int(rand()+.5)?"-":"";
 $coef1=int(rand(4))+1;
 if($coef1==1){$coef1=""}
@@ -93,9 +98,9 @@ if($coef2==1){$coef2=""}
 
 $sig3=int(rand()+.5)?"-":"+";
 $coef3=int(rand(4))+1;
-print $sig1,$coef1,"x",$exp1,$sig2,$coef2,"x",$exp2,$sig3,$coef3;
+print "\\sqrt[",$exp3,"]{",$sig1,$coef1,"x",$exp1,$sig2,$coef2,"x",$exp2,$sig3,$coef3;
 
-print "}=\$\$\n";
+print "}}=\$\$\n";
  $i++
 }
 
