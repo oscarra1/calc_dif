4f2a910b5c3fc0eb4ecd6dbf296dc0215080fbb8
Oscar Rendón Aldaraca <o_rendon@yahoo.com>
2017-03-24 04:58:23 -0600

    En la rama master  Cambios para hacer commit: 	modificado:    ejercicios.pl

--- ejercicios.pl
+++ ejercicios.pl
@@ -1,8 +1,8 @@
 #!/usr/bin/perl
 $exp1=int(rand(4)+2);
 $exp2=int(rand(4)+2);
-$sig11=int(rand())?"-":"";
+$sig1=int(rand()+.5)?"-":"";
 $coef1=int(rand(4))+1;
 if($coef1==1){$coef1=""}
 print " \\item \$\$\\underset\{x\\rightarrow -\\infty\}\{l\\acute\{\\imath\}m\} \\frac\{";
-print $sig11,$coef1,"x^$exp1+2x^",$exp1-1,"+2}{5x^2+3\}=\$\$\n";
+print $sig1,$coef1,"x^$exp1+2x^",$exp1-1,"+2}{5x^2+3\}=\$\$\n";
