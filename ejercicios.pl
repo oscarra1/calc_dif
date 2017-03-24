6425a0a220db71b322d1d378856eb17902357f50
Oscar Rendón Aldaraca <o_rendon@yahoo.com>
2017-03-24 06:21:25 -0600

    En la rama master  Cambios para hacer commit: 	modificado:    ejercicios.pl     Completado numerador

--- ejercicios.pl
+++ ejercicios.pl
@@ -1,6 +1,11 @@
 #!/usr/bin/perl
 $exp1=int(rand(4)+2);
-$exp2=int(rand(4)+2);
+if($exp1==2){
+ $exp1="^2";
+ $exp2="";}
+else{
+ $exp2="^".($exp1-1);
+ $exp1="^".$exp1}
 $sig1=int(rand()+.5)?"-":"";
 $coef1=int(rand(4))+1;
 if($coef1==1){$coef1=""}
@@ -11,6 +16,5 @@ if($coef2==1){$coef2=""}
 
 $sig3=int(rand()+.5)?"-":"+";
 $coef3=int(rand(4))+1;
-if($coef3==1){$coef3=""}
 print " \\item \$\$\\underset\{x\\rightarrow -\\infty\}\{l\\acute\{\\imath\}m\} \\frac\{";
-print $sig1,$coef1,"x^$exp1",$sig2,$coef2,"x^",$exp1-1,$sig3,$coef3,"}{5x^2+3\}=\$\$\n";
+print $sig1,$coef1,"x",$exp1,$sig2,$coef2,"x",$exp2,$sig3,$coef3,"}{5x^2+3\}=\$\$\n";
