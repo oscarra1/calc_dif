45027bd0d6ed57581b3d47424f017a62a2a2d2eb
Oscar Rendón Aldaraca <o_rendon@yahoo.com>
2017-03-24 04:02:04 -0600

    En la rama master  Cambios para hacer commit: 	modificado:    ejercicios.pl

--- ejercicios.pl
+++ ejercicios.pl
@@ -1,2 +1,2 @@
 #!/usr/bin/perl
-print " \item $$\underset{x\rightarrow -\infty}{l\acute{\imath}m} \frac{x^4+2x+2}{5x^2+3}=$$\n";
+print " \\item \$\$\\underset\{x\\rightarrow -\\infty\}\{l\\acute\{\\imath\}m\} \\frac\{x^4+2x+2}{5x^2+3\}=\$\$\n";
