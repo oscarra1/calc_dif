ca6a64dc3ac640c332e8c7094e25053f28960103
Oscar Rendón Aldaraca <o_rendon@yahoo.com>
2017-03-24 07:32:49 -0600

    En la rama master  Cambios para hacer commit: 	modificado:    Ejercicios24-mar-17.pdf 	modificado:    Ejercicios24-mar-17.tex 	modificado:    ejercicios.pl
         Primeras raices

--- ejercicios.pl
+++ ejercicios.pl
@@ -46,3 +46,56 @@ print $sig1,$coef1,"x",$exp1,$sig2,$coef2,"x",$exp2,$sig3,$coef3;
 print "}=\$\$\n";
  $i++
 }
+
+$i=0;
+while ($i<5){
+ $exp1=int(rand(4)+2);
+ if($exp1==2){
+  $exp1="^2";
+  $exp2="";}
+ else{
+  $exp2="^".($exp1-1);
+  $exp1="^".$exp1
+ }
+
+ $exp3=int(rand(4)+2);
+ if($exp3==2){
+  $exp3=""}
+
+$sig1=int(rand()+.5)?"-":"";
+$coef1=int(rand(4))+1;
+if($coef1==1){$coef1=""}
+
+$sig2=int(rand()+.5)?"-":"+";
+$coef2=int(rand(4))+1;
+if($coef2==1){$coef2=""}
+
+$sig3=int(rand()+.5)?"-":"+";
+$coef3=int(rand(4))+1;
+
+print " \\item \$\$\\underset\{x\\rightarrow -\\infty\}\{l\\acute\{\\imath\}m\} \\frac\{";
+print "\\sqrt[",$exp3,"]{",$sig1,$coef1,"x",$exp1,$sig2,$coef2,"x",$exp2,$sig3,$coef3,"}}{";
+
+$exp1=int(rand(4)+2);
+if($exp1==2){
+ $exp1="^2";
+ $exp2="";}
+else{
+ $exp2="^".($exp1-1);
+ $exp1="^".$exp1}
+$sig1=int(rand()+.5)?"-":"";
+$coef1=int(rand(4))+1;
+if($coef1==1){$coef1=""}
+
+$sig2=int(rand()+.5)?"-":"+";
+$coef2=int(rand(4))+1;
+if($coef2==1){$coef2=""}
+
+$sig3=int(rand()+.5)?"-":"+";
+$coef3=int(rand(4))+1;
+print $sig1,$coef1,"x",$exp1,$sig2,$coef2,"x",$exp2,$sig3,$coef3;
+
+print "}=\$\$\n";
+ $i++
+}
+
