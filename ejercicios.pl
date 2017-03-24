d10d5ca99f10c610e6da942ca57a4d534c93efb9
Oscar Rendón Aldaraca <o_rendon@yahoo.com>
2017-03-24 06:54:41 -0600

    En la rama master  Cambios para hacer commit: 	modificado:    Ejercicios24-mar-17.pdf 	modificado:    Ejercicios24-mar-17.tex 	modificado:    ejercicios.pl
           Realiza 5 primeros ejercicios

--- ejercicios.pl
+++ ejercicios.pl
@@ -1,12 +1,15 @@
 #!/usr/bin/perl
 
-$exp1=int(rand(4)+2);
-if($exp1==2){
- $exp1="^2";
- $exp2="";}
-else{
- $exp2="^".($exp1-1);
- $exp1="^".$exp1}
+$i=0;
+while ($i<5){
+ $exp1=int(rand(4)+2);
+ if($exp1==2){
+  $exp1="^2";
+  $exp2="";}
+ else{
+  $exp2="^".($exp1-1);
+  $exp1="^".$exp1
+ }
 $sig1=int(rand()+.5)?"-":"";
 $coef1=int(rand(4))+1;
 if($coef1==1){$coef1=""}
@@ -17,6 +20,7 @@ if($coef2==1){$coef2=""}
 
 $sig3=int(rand()+.5)?"-":"+";
 $coef3=int(rand(4))+1;
+
 print " \\item \$\$\\underset\{x\\rightarrow -\\infty\}\{l\\acute\{\\imath\}m\} \\frac\{";
 print $sig1,$coef1,"x",$exp1,$sig2,$coef2,"x",$exp2,$sig3,$coef3,"}{";
 
@@ -39,5 +43,6 @@ $sig3=int(rand()+.5)?"-":"+";
 $coef3=int(rand(4))+1;
 print $sig1,$coef1,"x",$exp1,$sig2,$coef2,"x",$exp2,$sig3,$coef3;
 
-
 print "}=\$\$\n";
+ $i++
+}
