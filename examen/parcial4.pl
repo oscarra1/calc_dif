9f94c469bf3effef5b448cd46805422cac860337
Oscar Rendón Aldaraca <oscar.rendonaldaraca@gmail.com>
2017-05-11 14:25:01 -0500

    En la rama prueba  Cambios para hacer commit: 	modificado:    parcial4.pdf 	modificado:    parcial4.pl 	modificado:    parcial4.tex

--- examen/parcial4.pl
+++ examen/parcial4.pl
@@ -161,6 +161,57 @@ while($j<45){#todos los exámenes
 
  print "\\item \$\$\\diff{}{x}(",$coef1,"x",$sig2,$coef2,"y)",$exp1,"\$\$\n";
 
+ #Derivadas de funciones trascendentales
+ @fun=("sen","cos","tan","ln");
+ $i=0;
+ while ($i<2){#funciones trascendentales
+   $exp1=int(rand(4)+2);
+   if($exp1==2){
+    $exp1="^x";
+    $exp2="";}
+   else{
+    $exp2="^".($exp1-1);
+    $exp1="^".$exp1
+   }
+   $sig1=int(rand()+.5)?"-":"";
+   $coef1=int(rand(4))+1;
+   if($coef1==1){$coef1=""}
+
+   $sig2=int(rand()+.5)?"-":"+";
+   $coef2=int(rand(4))+1;
+   if($coef2==1){$coef2=""}
+
+   $sig3=int(rand()+.5)?"-":"+";
+   $coef3=int(rand(4))+1;
+   $xx="x";
+   if($exp1=="^x"){$xx="e"};
+   $func=$fun[int(rand(2)+2)];
+   print " \\item \$\$ D_x \\left(\\;",$func,"\\left( \\frac\{";
+   print $sig1,$coef1,$xx,$exp1,$sig2,$coef2,"x",$exp2,$sig3,$coef3,"}{";
+
+   $exp1=int(rand(4)+2);
+   if($exp1==2){
+    $exp1="^2";
+    $exp2="";}
+   else{
+    $exp2="^".($exp1-1);
+    $exp1="^".$exp1
+   }
+   $sig1=int(rand()+.5)?"-":"";
+   $coef1=int(rand(4))+1;
+   if($coef1==1){$coef1=""}
+
+   $sig2=int(rand()+.5)?"-":"+";
+   $coef2=int(rand(4))+1;
+   if($coef2==1){$coef2=""}
+
+   $sig3=int(rand()+.5)?"-":"+";
+   $coef3=int(rand(4))+1;
+   print $sig1,$coef1,"x",$exp1,$sig2,$coef2,"x",$exp2,$sig3,$coef3;
+
+   print "}\\right)\\right)=\$\$\n";
+   $i++
+ }#final de funciones trascendentales
 
  print " \\end{enumerate}\n ";
  print " \\newpage\n";
